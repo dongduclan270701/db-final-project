9e7f911301c5fee8914f8ecfc2e9ed4b269d9bb7
dongduclan270701 <dongduclan27072001@gmail.com>
2023-01-30 02:51:24 +0700

    hash password

--- src/app.js
+++ src/app.js
@@ -1,44 +1,58 @@
 // Requiring module
 const express = require("express");
 const fs = require("fs");
+const bodyParser = require("body-parser")
+// const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
 var path = require('path');
 
 const app = express();
 
-function authentication(req, res, next) {
-	var authheader = req.headers.authorization;
-	console.log(req.headers);
+// function authentication(req, res, next) {
+// 	var authheader = req.headers.authorization;
+// 	console.log(req.headers);
 
-	if (!authheader) {
-		var err = new Error('You are not authenticated!');
-		res.setHeader('WWW-Authenticate', 'Basic');
-		err.status = 401;
-		return next(err)
-	}
+// 	if (!authheader) {
+// 		var err = new Error('You are not authenticated!');
+// 		res.setHeader('WWW-Authenticate', 'Basic');
+// 		err.status = 401;
+// 		return next(err)
+// 	}
 
-	var auth = new Buffer.from(authheader.split(' ')[1],
-	'base64').toString().split(':');
-	var user = auth[0];
-	var pass = auth[1];
+// 	var auth = new Buffer.from(authheader.split(' ')[1],
+// 	'base64').toString().split(':');
+// 	var user = auth[0];
+// 	var pass = auth[1];
 
-	if (user == 'admin' && pass == 'password') {
+// 	if (user == 'admin' && pass == 'password') {
 
-		// If Authorized user
-		next();
-	} else {
-		var err = new Error('You are not authenticated!');
-		res.setHeader('WWW-Authenticate', 'Basic');
-		err.status = 401;
-		return next(err);
-	}
+// 		// If Authorized user
+// 		next();
+// 	} else {
+// 		var err = new Error('You are not authenticated!');
+// 		res.setHeader('WWW-Authenticate', 'Basic');
+// 		err.status = 401;
+// 		return next(err);
+// 	}
 
-}
+// }
 
-// First step is the authentication of the client
-app.use(authentication)
-app.use(express.static(path.join(__dirname, 'public')));
+// // First step is the authentication of the client
+// app.use(authentication)
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Server setup
+
+// mongoose.connect()
+app.use('/', express.static(path.join(__dirname, 'static')))
+app.use(bodyParser.json())
+
+app.post('/api/register', async (req, res) =>{
+	// console.log(req.query.name)
+	console.log(await bcrypt.hash(req.query.name, 10))
+	res.json({ status: 'ok'})
+})
+
 app.listen((3000), () => {
 	console.log("Server is Running ");
 })
