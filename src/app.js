c3622bddbe6f214de3810cebdd8ad77484b371b5
dongduclan270701 <dongduclan27072001@gmail.com>
2023-03-16 17:08:09 +0700

    commit

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import { connectDB } from '*/config/db.js'
-import { User } from "*/model/user"
+// import { User } from "*/model/user"
 import { apiV1 } from '*/routes/v1'
 const express = require("express");
 // const fs = require("fs");
 const bodyParser = require("body-parser")
-const bcrypt = require("bcryptjs")
+// const bcrypt = require("bcryptjs")
 var path = require('path');
 var cors = require('cors')
 const app = express();
-const jwt = require("jsonwebtoken")
+// const jwt = require("jsonwebtoken")
 // Server setup
 
 connectDB()
